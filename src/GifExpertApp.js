63f5bf709367f9ec5573b7904d65a9e9a5d5c6c7
judaaaron <judaponce14@unitec.edu>
2022-04-04 17:20:26 -0600

    Finalizar

---
+++ src/GifExpertApp.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+export const GifExpertApp = () => {
+    
+   
+
+   const [categories, setCategories] = useState(['Programmer']);
+
+//    const handleAdd = ()=>{
+//         setCategories([...categories, 'Los 100']);
+    
+//    }
+    return (
+        <> 
+            <h2>GifExpertApp</h2>
+            <AddCategory setCategories={setCategories}/>
+
+           <hr/>
+          
+           <ol>
+               { 
+                categories.map(category =>(
+                    <GifGrid 
+                    key={category}
+                    category={category}
+                    
+                    />
+                 ))
+               }
+           </ol>
+        </>
+    )
+
+}
